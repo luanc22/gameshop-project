e66d0e0693ba35fee6553fada0b1b38ff872c937
Luan Cabral <luancabral5566@gmail.com>
2022-12-14 12:23:08 -0300

    feat: adds nodemon for auto-restart on changes

--- routes.js
+++ routes.js
@@ -25,11 +25,11 @@ const requestHandler = (req, res) => {
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
-      }
+      }s
       res.setHeader('Content-Type', 'text/html');
       res.write('<html>');
       res.write('<head><title>My First Page</title><head>');
-      res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+      res.write('<body><h1>Hello from my Node.js Server!!</h1></body>');
       res.write('</html>');
       res.end();
 };
